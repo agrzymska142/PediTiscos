3599f36c08123329bcceffc35b8fd09636c2b596
KamilKonefeld <54834569+KKonefeld@users.noreply.github.com>
2024-12-19 15:34:39 +0000

    Shop updates
    products by category
    product details
    layout update

---
+++ RCL/Data/DTO/ProductDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RCL.Data.DTO
+{
+    internal class ProductDTO
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public string? CategoryName { get; set; }
+    }
+}
---
+++ StoreManager/Data/Migrations/20241218221212_Migration1812.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StoreManager.Migrations
+{
+    /// <inheritdoc />
+    public partial class Migration1812 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: 1,
+                column: "OrderDate",
+                value: new DateTime(2024, 12, 17, 22, 12, 11, 157, DateTimeKind.Local).AddTicks(5159));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: 2,
+                column: "OrderDate",
+                value: new DateTime(2024, 12, 16, 22, 12, 11, 157, DateTimeKind.Local).AddTicks(6601));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: 1,
+                column: "OrderDate",
+                value: new DateTime(2024, 12, 15, 15, 13, 23, 299, DateTimeKind.Local).AddTicks(2061));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: 2,
+                column: "OrderDate",
+                value: new DateTime(2024, 12, 14, 15, 13, 23, 299, DateTimeKind.Local).AddTicks(3660));
+        }
+    }
+}
